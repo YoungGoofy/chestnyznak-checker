a14977a06c26b838baaf8754fa782730e884e332
Vlad Bichenkov <younggoofy@users.noreply.github.com>
2026-05-13 21:34:54 +0800

    bump version to 1.2.5

--- cischecker/__init__.py
+++ cischecker/__init__.py
@@ -1,5 +1,5 @@
 """CISChecker — проверка кодов маркировки «Честный Знак»."""
 
-APP_VERSION = "1.2.4"
+APP_VERSION = "1.2.5"
 APP_TITLE = "CISChecker — Проверка кодов маркировки"
 GITHUB_REPO = "YoungGoofy/chestnyznak-checker"
